55161ea2715b1cd7384e6518e15a17d3610df600
New User <newuser@News-iMac.local>
2024-04-17 14:51:06 +1000

    added options but have not applied yet

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,4 +1,5 @@
 #include "nn.hpp"
+#include <getopt.h>
 
 
 const float NN_INITIAL_WEIGHT = 0.5;
@@ -24,7 +25,51 @@ void train(const string tset_file)
     network.train(tset);
 }
 
-int main() 
+void process_args(int argc, char** argv) {
+    const char* const short_opts = "t:c:";
+    const option long_opts[] = {
+        {"config", required_argument, nullptr, 'c'},
+        {"tdir",   optional_argument, nullptr, 't'},
+        {"help",   no_argument,       nullptr, 'h'}
+    };
+
+     while (true)
+     {
+         const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+
+        if (-1 == opt)
+            break;
+
+        switch (opt) 
+        {
+            // Help is first option so that loop is automatically broken
+            case 'h':
+                cout << "print this help screen" << endl;
+                break;
+
+
+            // config file
+            case 'c':
+                //string config_file = string(optarg);
+                Logger::info("config set: " + string(optarg));
+                //config.load_file(string(optarg));
+                break;
+
+            // training set.
+            case 't':
+                //string tdata = string(optarg);
+                Logger::info("trainging set: " + string(optarg));
+                // training_set = string(optarg);
+                break;
+
+            default:
+                cout << "print this help screen" << endl;
+                break;
+        }
+     }
+}
+
+int main(int argc, char** argv) 
 {
     setup();
 
