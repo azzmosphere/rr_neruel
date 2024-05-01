c24e609a0ee7c2516388e4f0f61c8aee0b7a8bad
Aaron Spiteri <azzmosphere@gmail.com>
2024-05-01 13:25:17 +1000

    created server

--- src/main/rr-environment/main.cpp
+++ src/main/rr-environment/main.cpp
@@ -19,40 +19,42 @@
 
 using namespace std;
 
-using namespace std; 
-  
-int main() 
-{ 
-    // creating socket 
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0); 
-  
-    // specifying the address 
-    sockaddr_in serverAddress; 
-    serverAddress.sin_family = AF_INET; 
-    serverAddress.sin_port = htons(8080); 
-    serverAddress.sin_addr.s_addr = INADDR_ANY; 
-  
-    // binding socket. 
-    bind(serverSocket, (struct sockaddr*)&serverAddress, 
-         sizeof(serverAddress)); 
-  
-    // listening to the assigned socket 
-    listen(serverSocket, 5); 
-  
-    // accepting connection request 
-    int clientSocket 
-        = accept(serverSocket, nullptr, nullptr); 
-  
-    // recieving data 
-    char buffer[1024] = { 0 }; 
-    recv(clientSocket, buffer, sizeof(buffer), 0); 
+using namespace std;
 
-    cout << "Message from client: " << buffer << endl; 
-    string m =  "Message from client: " + string(buffer) + "\n";
-    send(clientSocket, m.c_str(), m.size(), 0);
-  
-    // closing the socket. 
-    close(serverSocket); 
-  
-    return 0; 
+int main()
+{
+    // creating socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // specifying the address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // binding socket.
+    bind(serverSocket, (struct sockaddr *)&serverAddress,
+         sizeof(serverAddress));
+
+    // listening to the assigned socket
+    listen(serverSocket, 5);
+
+    // recieving data
+    char buffer[1024] = {0};
+
+    while (1)
+    {
+        // accepting connection request
+        int clientSocket = accept(serverSocket, nullptr, nullptr);
+        recv(clientSocket, buffer, sizeof(buffer), 0);
+        cout << "Message from client: " << buffer << endl;
+        string m = "Message from client: " + string(buffer) + "\n";
+        send(clientSocket, m.c_str(), m.size(), 0);
+        close(clientSocket);
+    }
+
+    // closing the socket.
+    close(serverSocket);
+
+    return 0;
 }
