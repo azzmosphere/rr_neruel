fd25ab5dc8293ef4f98e428a92874a25389593fd
Aaron Spiteri <azzmosphere@gmail.com>
2024-05-13 15:54:36 +1000

    added common header file

--- src/main/rr-environment/components/L298Motor.cpp
+++ src/main/rr-environment/components/L298Motor.cpp
@@ -1,80 +1,43 @@
 #include "L298Motor.hpp"
 
-void L298Motor::setIn1(uint32_t in)
+const int IN1 = GPIO_PIN23;
+const int IN2 = GPIO_PIN24;
+const int ENA = PWM0;
+
+const int IN3 = GPIO_PIN27;
+const int IN4 = GPIO_PIN17;
+const int ENB = PWM1;
+
+void L298Motor::driveWheels(int in1, int in2, int in3,  int in4, int ena, int enb)
 {
-    _in1 = in;
-}
+    digitalWrite(IN1, in1);
+    digitalWrite(IN2, in2);
 
-void L298Motor::setIn2(uint32_t in)
-{
-    _in2 = in;
-}
+    digitalWrite(IN3, in3);
+    digitalWrite(IN4, in4);
 
-void L298Motor::setIn3(uint32_t in)
-{
-    _in3 = in;
-}
-
-void L298Motor::setIn4(uint32_t in)
-{
-    _in4 = in;
-}
-
-void L298Motor::setEnA(uint32_t in)
-{
-    _enA = in;
-}
-
-
-void L298Motor::setEnB(uint32_t in)
-{
-    _enB = in;
-}
-
-void L298Motor::driveWheels(uint32_t in1, uint32_t in2, uint32_t in3,  uint32_t in4, uint32_t ena, uint32_t enb)
-{
-    digitalWrite(_in1, in1);
-    digitalWrite(_in2, in2);
-
-    digitalWrite(_in3, in1);
-    digitalWrite(_in4, in2);
-    
-    pwmWrite(_enA, ena);
-    pwmWrite(_enB, ena);
+    // range is 0 to 1024
+    pwmWrite(ENA, (ena > MAX_PWM_VAL) ? MAX_PWM_VAL : ena);
+    pwmWrite(ENB, (enb > MAX_PWM_VAL) ? MAX_PWM_VAL : enb);
 }
 
 void L298Motor::setup()
 {
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(ENA, PWM_OUTPUT);
 
-    setEnA(ENA);
-    setEnB(ENB);
-
-    setIn1(IN1);
-    setIn2(IN2);
-    setIn3(IN3);
-    setIn4(IN4);
-
-    // softPwmCreate(_enA, 0, 100);
-    // softPwmCreate(_enB, 0, 100);
-
-    pinMode(_in1, OUTPUT);
-    pinMode(_in2, OUTPUT);
-
-    pinMode(_in3, OUTPUT);
-    pinMode(_in4, OUTPUT);
-
-    pinMode(_enA, PWM_OUTPUT);
-    pinMode(_enB, PWM_OUTPUT);
-
-        // set the PWM mode to Mark Space
-    pwmSetMode(PWM_MODE_BAL);
-    pwmSetClock (3840);  // 19.2 Mhz divided by 3840 is 5 Khz.
-    pwmSetRange (2500); 
-
-
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENB, PWM_OUTPUT);
 }
 
 void L298Motor::teardown()
 {
-
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    pwmWrite(ENA, 0);
+    pwmWrite(ENB, 0);
 }
--- src/main/rr-environment/components/L298Motor.hpp
+++ src/main/rr-environment/components/L298Motor.hpp
@@ -2,49 +2,18 @@
 #define L298MOTOR
 
 #include <stdint.h>
-#include "wiringiface.hpp"
+#include "ComponentsCommon.hpp"
 #include "Logger.hpp"
 
-
-const uint32_t IN1 = 24; // orange
-const uint32_t IN2 = 23; // 
-const uint32_t ENA = 12; // pysical pin 32  GPIO12 
-
-const uint32_t IN3 = 5; // 
-const uint32_t IN4 = 6;
-const uint32_t ENB = 13;
-
-
+#define MAX_PWM_VAL 1024
 
 class L298Motor {
-    private:
-        uint32_t _in1;
-        uint32_t _in2;
-
-        uint32_t _in3;
-        uint32_t _in4;
-
-        uint32_t _enA;
-        uint32_t _enB;
-
     public:
-        void setIn1(uint32_t in);
-
-        void setIn2(uint32_t in);
-
-        void setIn3(uint32_t in);
-
-        void setIn4(uint32_t in);
-
-        void setEnA(uint32_t in);
-
-        void setEnB(uint32_t in);
-
         void setup();
 
         void teardown();
 
-        void driveWheels(uint32_t in1, uint32_t in2, uint32_t in3,  uint32_t in4, uint32_t ena, uint32_t enb);
+        void driveWheels(int in1, int in2, int in3,  int in4, int ena, int enb);
 };
 
 #endif
