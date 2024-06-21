7eeb6e12660e0225f223d9aefd1ee27b19cea9b5
Aaron Spiteri <azzmosphere@gmail.com>
2024-06-21 17:01:31 +1000

    keeping existing changes

--- resources/enviroment-test-scripts/move-forward-1sec.py
+++ resources/enviroment-test-scripts/move-forward-1sec.py
@@ -1,11 +1,24 @@
 from telnetlib import Telnet
 import time
 
-with Telnet('192.168.1.14', 8080) as tn:
-    tn.interact()
+tn = Telnet('192.168.1.14', 8080)
 
-tn.write(b'{ "op" : 1, "actions": [ { "oid": 1, "name": "moveforward", "value": 500 } ] }')
+
+# MOVE FORWARD
+# Relatative range is between is between 1000 - 2000
+# 92cm
+# tn.write(b'{ "op" : 1, "actions": [ { "oid": 1, "name": "moveforward", "value": 1000 } ] }')
+
+# 119cm
+# tn.write(b'{ "op" : 1, "actions": [ { "oid": 1, "name": "moveforward", "value": 2000 } ] }')
+
+# time.sleep(1)
+
+# MOVE LEFT 
+tn.write(b'{ "op" : 1, "actions": [ { "oid": 3, "name": "left", "value": 1500 } ] }')
 time.sleep(1)
+
+tn = Telnet('192.168.1.14', 8080)
 tn.write(b'{ "op" : 1, "actions": [ { "oid": 2, "name": "stop", "value": 500 } ] }')
 
 tn.close()
